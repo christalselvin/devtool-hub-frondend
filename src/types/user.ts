f774c757a9426462be2e8697b49fe8c02cb2986f
CHRSTAL SELVIN <127867279+christalselvin@users.noreply.github.com>
2026-08-22 12:40:12 +0530

    Add RBAC data to frontend user type

--- src/types/user.ts
+++ src/types/user.ts
@@ -1,17 +1,14 @@
 export interface User {
   id: string;
-
   first_name: string;
-
   last_name?: string;
-
   username: string;
-
   email: string;
-
   phone?: string;
-
+  profile_image?: string;
   is_active: boolean;
-
   is_verified: boolean;
+  is_superuser?: boolean;
+  roles: string[];
+  permissions: string[];
 }
